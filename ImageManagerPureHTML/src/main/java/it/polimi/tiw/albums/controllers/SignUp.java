5395d8fc2e4ab2dd36fe6e2e2eae937057712004
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-12-15 19:28:31 +0100

    Implemented Input checks and thymeleaf context generation in SignUp controller

--- ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/controllers/SignUp.java
+++ ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/controllers/SignUp.java
@@ -1,6 +1,7 @@
 package it.polimi.tiw.albums.controllers;
 
 import java.io.IOException;
+import java.io.Writer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,11 +14,27 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.IWebApplication;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import it.polimi.tiw.albums.beans.User;
+import it.polimi.tiw.albums.daos.UserDAO;
+import it.polimi.tiw.albums.utils.InputSanitizer;
 
 @WebServlet("/SignUp")
 public class SignUp extends HttpServlet{
-	private static final long serialVersionUID = 1L;
 	//ATTRIBUTES
+	private static final long serialVersionUID = 1L;
+	private ITemplateEngine templateEngine;
+	private JakartaServletWebApplication application;
 	private Connection conn;
 	
 	
@@ -32,6 +49,10 @@ public class SignUp extends HttpServlet{
 	//SERVLET METHODS
 	@Override
 	public void init() throws ServletException {
+		this.application = JakartaServletWebApplication.buildApplication(getServletContext());
+        this.templateEngine = buildTemplateEngine(this.application);
+        
+        
 		try {
 			ServletContext context = getServletContext();
 			String driver = context.getInitParameter("dbDriver");
@@ -46,11 +67,40 @@ public class SignUp extends HttpServlet{
 		} catch (SQLException e) {
 			throw new UnavailableException("Couldn't get db connection");
 		}
+		
+	}
+	
+	//HELPER METHOD
+	private ITemplateEngine buildTemplateEngine(final IWebApplication application) {
+		final WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(application);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setSuffix(".html");
+        final TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        
+        return templateEngine;
+	}
+	
+	
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String path = "/WEB-INF/Sign-Up.html";
+		ServletContext servletContext = getServletContext();
+		
+		final IWebExchange webExchange = this.application.buildExchange(request, response);
+		WebContext ctx = new WebContext(webExchange, webExchange.getLocale());
+		final Writer writer = response.getWriter();
+		
+		templateEngine.process(path, ctx, writer);
 	}
 	
 	
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		boolean validCredentials = true;
+		
+		//Retrieve inputs
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String repeatPassword = request.getParameter("repeatPassword");
@@ -58,15 +108,87 @@ public class SignUp extends HttpServlet{
 		
 		String error = null;
 		
+		
+		//Sanitize Inputs
+		if(!InputSanitizer.isValidUsername(username)) {
+			error = "Missing or wrong username";
+			validCredentials = false;
+		}
+		else if(!InputSanitizer.isVaildEmail(email)) {
+			error = "Missing or wrong email";
+			validCredentials = false;
+		}
+		else if(!InputSanitizer.isValidPassword(password)) {
+			error = "Missing or wrong password";
+			validCredentials = false;
+		}
+		else if(repeatPassword == null || repeatPassword.isBlank() || repeatPassword.isEmpty()) {
+			error = "Missing repeat password";
+			validCredentials = false;
+		}
+		else if(!password.equals(repeatPassword)) {
+			error = "Passwords don't match";
+			validCredentials = false;
+		}
+		
+		
+		
+		else {
+			// Check if database already contains provided username or email
+			UserDAO userDao = new UserDAO(conn);
+			try {
+				if (!userDao.isUsernameAvailable(username)) {
+					error = "Username is not available";
+					validCredentials = false;
+				}
+
+				else if (!userDao.isEmailAvailable(email)) {
+					error = "Email is not available";
+					validCredentials = false;
+				}
+			} 
+			catch (SQLException e) {
+				e.printStackTrace(); // for debugging
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database access failed");
+			}
+		}
+		
+		
+		
+		if(validCredentials == false) {
+			String path = "/WEB-INF/Sign-Up.html";
+			final IWebExchange webExchange = this.application.buildExchange(request, response);
+			WebContext ctx = new WebContext(webExchange, webExchange.getLocale());
+			ctx.setVariable("error", error);
+			final Writer writer = response.getWriter();
+			
+			templateEngine.process(path, ctx, writer);
+		}
+		else {
+			//Credentials are valid, create user and redirect to home page
+			UserDAO userDao = new UserDAO(conn);
+			try {
+				userDao.createUser(username, email, password);
+				User user = userDao.getUserByCredentials(username, password);
+				//Add user information to html session
+				HttpSession s = request.getSession();
+				s.setAttribute("user", user);
+				
+				
+				String homePath = request.getServletContext().getContextPath() + "/Home";
+				response.sendRedirect(homePath);	
+			}
+			catch (SQLException e) {
+				e.printStackTrace(); // for debugging
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database access failed");
+			}
+		}
+		
+		return;
 	}
+
 	
 	
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		String path = "/WEB-INF/Sign-Up.html";
-		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-		dispatcher.forward(request, response);
-	}
 }
 
 
