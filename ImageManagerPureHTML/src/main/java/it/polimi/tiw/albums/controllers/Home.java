a9a10e3a789201b190038ea01eb66f62c9ab3139
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-12-17 20:15:15 +0100

    Updated Home servlet to support Thymeleaf

--- ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/controllers/Home.java
+++ ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/controllers/Home.java
@@ -1,10 +1,24 @@
 package it.polimi.tiw.albums.controllers;
 
 import java.io.IOException;
+import java.io.Writer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.IWebApplication;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import it.polimi.tiw.albums.beans.Album;
+import it.polimi.tiw.albums.beans.User;
+import it.polimi.tiw.albums.daos.AlbumDAO;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
@@ -13,12 +27,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/Home")
 public class Home extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 	// ATTRIBUTES
+	private static final long serialVersionUID = 1L;
 	private Connection conn;
+	private ITemplateEngine templateEngine;
+	private JakartaServletWebApplication application;
 
 	
 	
@@ -32,6 +49,9 @@ public class Home extends HttpServlet {
 	// SERVLET METHODS
 	@Override
 	public void init() throws ServletException {
+		this.application = JakartaServletWebApplication.buildApplication(getServletContext());
+		this.templateEngine = buildTemplateEngine(this.application);
+
 		try {
 			ServletContext context = getServletContext();
 			String driver = context.getInitParameter("dbDriver");
@@ -46,13 +66,49 @@ public class Home extends HttpServlet {
 		} catch (SQLException e) {
 			throw new UnavailableException("Couldn't get db connection");
 		}
+
+	}
+
+	// HELPER METHOD
+	private ITemplateEngine buildTemplateEngine(final IWebApplication application) {
+		final WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(application);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setSuffix(".html");
+		final TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+
+		return templateEngine;
 	}
 	
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = "/WEB-INF/Home.html";
-		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-		dispatcher.forward(request, response);
+		
+		HttpSession s = request.getSession();
+		User user = (User) s.getAttribute("user");
+		AlbumDAO albumDao = new AlbumDAO(conn);
+		List<Album> myAlbums = null;
+		List<Album> otherAlbums = null;
+		
+		try {
+			myAlbums = albumDao.getAlbumsByUser(user.getId());
+			otherAlbums = albumDao.getAlbumsByOthers(user.getId());
+		}
+		catch (SQLException e) {
+			e.printStackTrace(); // for debugging
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database access failed");
+		}
+		
+		
+		
+		final IWebExchange webExchange = this.application.buildExchange(request, response);
+		WebContext ctx = new WebContext(webExchange, webExchange.getLocale());
+		ctx.setVariable("myAlbums", myAlbums);
+		ctx.setVariable("otherAlbums", otherAlbums);
+		
+		final Writer writer = response.getWriter();
+		
+		templateEngine.process(path, ctx, writer);
 	}
 }
