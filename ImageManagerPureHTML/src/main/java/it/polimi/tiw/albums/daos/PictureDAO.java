bb1a9464731d594cc04de10f2b058c6ff381d27b
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-12-22 13:05:16 +0100

    Implemented method to add new pictures to database in PictureDAO

--- ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/daos/PictureDAO.java
+++ ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/daos/PictureDAO.java
@@ -53,7 +53,43 @@ private Connection con;
 	
 	
 	
-	
+	public int createPicture(int albumId, int uploader, String path, String thumbnailPath, String title, String description, Date date)throws SQLException{
+		int pictureId = -1;
+		String query ="INSERT INTO Picture (path, thumbnailPath, title, description, uploadDate, uploader) VALUES (?, ?, ?, ?, ?, ?);";
+		
+		//Add picture to database
+		try(PreparedStatement pstat = con.prepareStatement(query)){
+			pstat.setString(1, path);
+			pstat.setString(2, thumbnailPath);
+			pstat.setString(3, title);
+			pstat.setString(4, description);
+			pstat.setObject(5, date.toInstant().atZone(ZoneId.of("Europe/Rome")).toLocalDate());
+			pstat.setInt(6, uploader);
+			pstat.executeUpdate();
+		}
+		
+		//Retrieve database-generated id
+		query = "SELECT MAX(id) FROM Picture WHERE uploader = ?;";
+		try(PreparedStatement pstat = con.prepareStatement(query)){
+			pstat.setInt(1, uploader);
+			
+			try(ResultSet qres = pstat.executeQuery()){
+				while(qres.next()){
+					pictureId = qres.getInt("MAX(id)");
+				}
+			}
+		}
+		
+		//Link picture to album
+		query = "INSERT INTO Album_Picture (pictureId, albumId) VALUES (?, ?)";
+		try(PreparedStatement pstat = con.prepareStatement(query)){
+			pstat.setInt(1, pictureId);
+			pstat.setInt(2, albumId);
+			pstat.executeUpdate();
+		}
+		
+		return pictureId;
+	}
 }
 
 
