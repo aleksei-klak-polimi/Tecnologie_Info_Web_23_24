304daa007c89285095d688983c5fdea49d08e05a
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2024-12-23 00:09:23 +0100

    Fixed errors in CommentDAO queries and wrong import import java.sql.Date; instead of import java.util.Date;

--- ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/daos/CommentDAO.java
+++ ImageManagerPureHTML/src/main/java/it/polimi/tiw/albums/daos/CommentDAO.java
@@ -1,12 +1,12 @@
 package it.polimi.tiw.albums.daos;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import it.polimi.tiw.albums.beans.Comment;
@@ -26,7 +26,7 @@ public class CommentDAO {
 	
 	public List<Comment> getCommentsByPicture(int pictureId) throws SQLException {
 		List<Comment> comments = new ArrayList<Comment>();
-		String query ="SELECT C.id, C.body, C.pictureId, C.postDate, U.username FROM Comment C JOIN User U ON C.author = U.id WHERE pictureId = ?";
+		String query ="SELECT C.id, C.body, C.pictureId, C.postDate, U.username FROM Comment C JOIN User U ON C.author = U.id WHERE pictureId = ? ORDER BY C.postDate ASC, C.id ASC";
 		
 		try(PreparedStatement pstat = con.prepareStatement(query)){
 			pstat.setInt(1, pictureId);
@@ -54,7 +54,7 @@ public class CommentDAO {
 		
 		try(PreparedStatement pstat = con.prepareStatement(query)){
 			pstat.setString(1, body);
-			pstat.setInt(2, authorId);
+			pstat.setInt(2, pictureId);
 			pstat.setInt(3, authorId);
 			pstat.setObject(4, date.toInstant().atZone(ZoneId.of("Europe/Rome")).toLocalDate());
 			pstat.executeUpdate();
