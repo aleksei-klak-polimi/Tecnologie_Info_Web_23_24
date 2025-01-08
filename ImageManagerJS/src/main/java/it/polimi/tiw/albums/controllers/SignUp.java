107775a4afd3622439e9c33d15cf2ad4815702ac
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2025-01-08 18:40:02 +0100

    Added SignUp page with Java controller and JavaScript front end scripts.

---
+++ ImageManagerJS/src/main/java/it/polimi/tiw/albums/controllers/SignUp.java
@@ -0,0 +1,154 @@
+package it.polimi.tiw.albums.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.UnavailableException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+
+import it.polimi.tiw.albums.beans.User;
+import it.polimi.tiw.albums.controllers.helpers.DBConnector;
+import it.polimi.tiw.albums.daos.UserDAO;
+import it.polimi.tiw.albums.utils.InputSanitizer;
+
+@WebServlet("/SignUp")
+@MultipartConfig(
+		fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+		maxFileSize = 1024 * 1024 * 10,      // 10 MB
+		maxRequestSize = 1024 * 1024 * 100   // 100 MB
+)
+public class SignUp extends HttpServlet{
+	//ATTRIBUTES
+	private static final long serialVersionUID = 1L;
+	private Connection conn;
+	
+	
+	
+	//CONSTRUCTOR
+	public SignUp() {
+		super();
+	}
+	
+	
+	
+	//SERVLET METHODS
+	@Override
+	public void init() throws ServletException {
+		try {
+			conn = DBConnector.getConnection(getServletContext());
+
+		} catch (ClassNotFoundException e) {
+			throw new UnavailableException("Can't load database driver");
+		} catch (SQLException e) {
+			throw new UnavailableException("Couldn't get db connection");
+		}
+		
+	}
+	
+	
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String path = "/WEB-INF/Sign-Up.html";
+		ServletContext sc = getServletContext();
+		sc.getRequestDispatcher(path).forward(request, response);
+	}
+	
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		//Retrieve inputs
+		String username = null;
+		String password = null;
+		String repeatPassword = null;
+		String email = null;
+		
+		for (Part part : request.getParts()) {
+            if ("username".equals(part.getName())) {
+                username = new String(part.getInputStream().readAllBytes());
+            } else if ("password".equals(part.getName())) {
+                password = new String(part.getInputStream().readAllBytes());
+            } else if ("repeatPassword".equals(part.getName())) {
+            	repeatPassword = new String(part.getInputStream().readAllBytes());
+            } else if ("email".equals(part.getName())) {
+            	email = new String(part.getInputStream().readAllBytes());
+            }
+        }
+		
+		try {
+			UserDAO userDao = new UserDAO(conn);
+			// Sanitize Inputs
+			if (!InputSanitizer.isValidUsername(username)) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().write("Missing or wrong username.");
+				return;
+			}
+			else if (!InputSanitizer.isVaildEmail(email)) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().write("Missing or wrong email.");
+				return;
+			}
+			else if (!InputSanitizer.isValidPassword(password)) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().write("Missing or malformed password.");
+				return;
+			}
+			else if (repeatPassword == null || repeatPassword.isBlank() || repeatPassword.isEmpty()) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().write("Missing repeat password.");
+				return;
+			}
+			else if (!password.equals(repeatPassword)) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().write("Passwords don't match.");
+				return;
+			}
+			else {
+				// Check if database already contains provided username or email
+				if (!userDao.isUsernameAvailable(username)) {
+					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+					response.getWriter().write("Username is not available.");
+					return;
+				} else if (!userDao.isEmailAvailable(email)) {
+					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+					response.getWriter().write("Email is not available.");
+					return;
+				}
+
+			}
+
+			// Credentials are valid, create user and redirect to home page
+			userDao.createUser(username, email, password);
+			User user = userDao.getUserByCredentials(username, password);
+			// Add user information to html session
+			HttpSession s = request.getSession();
+			s.setAttribute("user", user);
+
+			String homePath = request.getServletContext().getContextPath() + "/Home";
+			response.sendRedirect(homePath);
+		} catch (SQLException e) {
+			e.printStackTrace(); // for debugging
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Database access failed");
+		}
+
+		return;
+	}
+
+	
+	
+}
+
+
+
+
+
+
---
+++ ImageManagerJS/src/main/webapp/scripts/SignUp.js
@@ -0,0 +1,51 @@
+/**
+ * SignUp logic management
+ */
+(function (){
+	//Clean-up place-holder values
+	var errorDiv = document.getElementsByClassName("errorText")[0];
+	var errorParent = errorDiv.parentNode;
+	errorParent.removeChild(errorDiv);
+	
+	document.getElementById("signUpBtn").addEventListener("click", (e) => signUp(e))
+	
+	//Onclick function
+	function signUp(e) {
+		e.preventDefault();
+		var form = e.target.closest("form");
+		if (form.checkValidity() && matchingPasswords()) {
+			makeCall("POST", 'SignUp', e.target.closest("form"), callBackFunc);
+		}
+		else {
+			form.reportValidity();
+		}
+	}
+	
+	function matchingPasswords(){
+		var pwd = document.getElementById("password").textContent;
+		var repeatPwd = document.getElementById("repeatPassword").textContent;
+		
+		if(pwd !== repeatPwd){
+			//Passwords don't match
+			errorDiv.textContent = "Passwords don't match.";
+			errorParent.appendChild(errorDiv);
+			return false;
+		}
+		return true;
+	}
+	
+	//CallBack function
+	function callBackFunc(x){
+		if (x.readyState == XMLHttpRequest.DONE){
+			var message = x.responseText;
+			if(x.status == 400 || x.status == 401 || x.status == 402){
+				errorDiv.textContent = message;
+				errorParent.appendChild(errorDiv);
+				alert(message);
+			}
+		}
+	}
+})();
+
+
+
