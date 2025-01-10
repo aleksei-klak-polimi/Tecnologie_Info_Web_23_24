8596e03d2af537a6ed46e7e22757af329158f32f
aleksei-klak-polimi <aleksei.klak@mail.polimi.it>
2025-01-10 18:09:25 +0100

    Updated utils.js to expose more specific functions for server requests.

--- ImageManagerJS/src/main/webapp/scripts/utils.js
+++ ImageManagerJS/src/main/webapp/scripts/utils.js
@@ -2,11 +2,40 @@
  * Script to handle sending of xmlhttp form post requests to the server
  */
 
-function makeCall(method, url, formElement, callBack){
-	var req = new XMLHttpRequest();
+function postForm(url, formElement, callBack, reset){
+	let req = new XMLHttpRequest();
 	req.onreadystatechange = function(){callBack(req)};
+	req.open('POST', url);
 	
-	var formData = new FormData(formElement);
-	req.open(method, url);
+	var formData = new FormData(formElement);	
 	req.send(formData);
+
+	if (formElement !== null && reset === true) {
+		formElement.reset();
+	}
 }
+
+function getRequest(url, callBack){
+	let req = new XMLHttpRequest();
+	req.onreadystatechange = function(){callBack(req)};
+	req.open('GET', url);
+	req.send();
+}
+
+function getPage(url, callback){
+	let req = new XMLHttpRequest();
+	req.onload = function(){callback(req)};
+	req.open('GET', url);
+	req.responseType = 'document';
+	req.send();
+}
+
+
+
+
+
+
+
+
+
+
